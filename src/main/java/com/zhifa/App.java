74fd989820063e2170c9dc4d7981e7b219a27249
xiaozhi止水 <877657682@qq.com>
2019-07-16 19:52:22 +0800

    ............

---
+++ src/main/java/com/zhifa/App.java
@@ -0,0 +1,33 @@
+package com.zhifa;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        System.setProperty("hadoop.home.dir", "D:\\hadoop-common-2.2.0-bin");
+        System.out.println( "Hello World!" );
+        String uri = "hdfs://47.100.45.101:9000/";
+        Configuration conf = new Configuration();
+
+        FileSystem fs = FileSystem.get(URI.create(uri),conf,"root");
+        //列出hdfs上 /input 目录下的所有文件
+        FileStatus[] statuses = fs.listStatus(new Path("/"));
+        for (FileStatus status:statuses){
+            System.out.println(status);
+        }
+
+
+    }
+}
